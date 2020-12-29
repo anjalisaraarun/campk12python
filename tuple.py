5ecef3739aa6e9c8674640921688d23c39415635
Arunkumar Chacko <carunkumar@gmail.com>
2020-12-29 20:47:48 +0530

    My commmit

---
+++ tuple.py
@@ -0,0 +1,9 @@
+colors = ('blue','yellow','cyan','red','purle','golden','bonesana','silver')
+print(len(colors))
+print(type(colors))
+print(colors[-2])
+print(colors[1:7])
+a = list(colors)
+a [3] = 'black'
+colors = tuple (a)
+print(a)
